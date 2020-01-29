295fda459b2ee643fb79d152ba7e260f8f37f643
JustinMarshall98 <58100273+JustinMarshall98@users.noreply.github.com>
2020-01-28 20:59:21 -0500

    Starting work on formSubmission

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import formSubmission from './formSubmission';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(formSubmission(), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

68868a133b11260ca0fb1cc5e7e9ddc52c8ae5bb
Leopoldine Vassiliou <lvassili@uwo.ca>
2020-03-22 17:18:59 -0400

    visualize endpoints

--- routes/DashboardRa.js
+++ routes/DashboardRa.js
@@ -6,29 +6,35 @@ const db = require("../database/invest-ed_db")
 
 dashboard.use(cors())
 
+//GET from from DB
 dashboard.get('/form', (req, res) =>{
     //req.query.tagNum
     
     //res.send("hello!")
 })
 
+//GET form from temp DB
 dashboard.get('/form-temp', (req, res) =>{
     //res.send("hello!")
 })
 
+//POST new form to temp DB
 dashboard.post('/new-form-temp', (req, res) =>{
     //res.send("hello!")
 })
 
+//POST modified form to temp DB
 dashboard.post('/modified-form-temp', (req, res) =>{
     //res.send("hello!")
 })
 
-dashboard.post('/new-form-temp', (req, res) =>{
+//POST new form to DB
+dashboard.post('/new-form', (req, res) =>{
     //res.send("hello!")
 })
 
-dashboard.post('/modified-form-temp', (req, res) =>{
+//Post modified form to DB
+dashboard.post('/modified-form', (req, res) =>{
     //res.send("hello!")
 })
 

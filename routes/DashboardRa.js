47f0c8518c361e5dedcd1741c72a87b86f610c40
Connor Cozens <ccozens2@uwo.ca>
2020-03-22 18:53:14 -0400

    creating changes for remote database testing

--- routes/DashboardRa.js
+++ routes/DashboardRa.js
@@ -8,8 +8,56 @@ dashboardRA.use(cors())
 
 //GET from from DB
 dashboardRA.get('/form', (req, res) =>{
-    //req.query.tagNum
+    tagnum = req.query.tagNum
     
+    iData = db.initiative.findAll({
+        where: {
+            tagNumber: tagnum
+        }
+    });
+
+    console.log(iData)
+
+    const form = {
+        // Funder Single Types
+        fName: db.funder.funderName,
+        fWebsite: db.funder.funderWebsite,
+        fProfitMotive: db.funder.funderProfitMotive,
+        fImpactInvesting: db.funder.impactInvesting,
+        fOrganizationalForm: db.funder.organizationForm,
+        // Funder Multivalued Types
+        fAsiaBases: db.funderAsiaBases.asiaBase,
+        fAsiaOperations: db.funderAsiaOperations.asiaOperatons,
+        fEducationSubsectors: db.funderEducationSubsectors.educationSubsector,
+        fInternationalBases: db.funderInternationalBases.baseLocation,
+        fOrganizationalTraits: db.funderOrganizationTraits.trait,
+        // Implementors Single Types
+        imName: db.implementor.implementorName,
+        imProfitMotive: db.implementor.profitMotive,
+        // Initiative Single Types
+        inTagNumber: db.initiative.tagNumber,
+        inName: db.initiative.initiativeName,
+        inWebsite: db.initiative.initiativeWebsite,
+        inTargetsWomen: db.initiative.targetsWomen,
+        inStartYear: db.initiative.startYear,
+        inEndYear: db.initiative.endYear,
+        inDescription: db.initiative.description,
+        inMainProgrammingArea: db.initiative.mainProgrammingArea,
+        inMainProgrammingActivity: db.initiative.mainProgrammingActivity,
+        inFeeToAccess: db.initiative.feeToAccess,
+        // Initiative Multivalued Types
+        inCountryOfOperation: db.initiativeCountryOfOperation.country,
+        inEducationSubsector: db.initiativeEducationSubsectors.educationSubsector,
+        inFundingSource: db.initiativeFundingSource.sourceOfFunding,
+        inLaunchCountry: db.initiativeLaunchCountry.launchCountry,
+        inMainEducationSubsector: db.initiativeMainEducationSubsector.mainEducationSubsector,
+        inMonitoredOutcomes: db.initiativeMonitoredOutcomes.monitoredOutcome,
+        inProgrammingActivities: db.initiativeProgrammingActivities.programmingActivity,
+        inRegion: db.initiativeRegion.region,
+        inTargetGeography: db.initiativeTargetGeography.targetGeography,
+        inTargetPopulationSubsector: db.initiativeTargetPopulationSector.targetPopulationSector,
+        inTargetSchoolManagementType: db.initiativeTargetSchoolManagement.targetSchoolManagementType
+    }
     //res.send("hello!")
 })
 

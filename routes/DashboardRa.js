d91c2dd82163832fb21981cacef34e8e066c0449
Connor Cozens <ccozens2@uwo.ca>
2020-03-22 17:16:45 -0400

    Added distinction to databases.

--- routes/DashboardRa.js
+++ routes/DashboardRa.js
@@ -6,21 +6,22 @@ const db = require("../database/invest-ed_db")
 
 dashboard.use(cors())
 
+//    FOR ACCESSING DATABASE DIRECTLY
 dashboard.get('/form', (req, res) =>{
     //req.query.tagNum
     
     //res.send("hello!")
 })
+dashboard.post('/new-form', (req, res) =>{
+    //res.send("hello!")
+})
 
+dashboard.post('/modified-form', (req, res) =>{
+    //res.send("hello!")
+})
+
+//    FOR ACCESSING TEMP DATABASE
 dashboard.get('/form-temp', (req, res) =>{
-    //res.send("hello!")
-})
-
-dashboard.post('/new-form-temp', (req, res) =>{
-    //res.send("hello!")
-})
-
-dashboard.post('/modified-form-temp', (req, res) =>{
     //res.send("hello!")
 })
 
